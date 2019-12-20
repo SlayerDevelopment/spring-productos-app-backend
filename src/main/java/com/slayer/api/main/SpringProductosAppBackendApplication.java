9bf4843c2d61695985e3f7f0cf777897b51bcea3
Bryan Antonio Alvarado Villalobos <bryan.amazon.alvarado@gmail.com>
2019-12-20 12:25:23 -0500

    Commit Inicial

---
+++ src/main/java/com/slayer/api/main/SpringProductosAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.slayer.api.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProductosAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProductosAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slayer/api/main/SpringProductosAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slayer.api.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProductosAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
